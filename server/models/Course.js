0970487a329dfa4ba9bdec2fe0aa31b3ae8ef1a4
sonu-sahani <sonusahani.tech@gmail.com>
2026-09-23 22:46:58 +0530

    fix: update courseRatings schema to require userId and rating fields

--- server/models/Course.js
+++ server/models/Course.js
@@ -32,11 +32,11 @@ const courseSchema = new mongoose.Schema(
     discount: { type: Number, required: true, min: 0, max: 100, default: 0 },
     courseContent: [chapterSchema],
     courseRatings: [
-      {
-        userId: { type: String, unique: true },
-        rating: { type: Number, min: 1, max: 5 },
-      },
-    ],
+  {
+    userId: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+  },
+],
     educator: { type: String, ref: 'User', required: true },
     enrolledStudents: [{ type: String, ref: 'User' }],
   },
